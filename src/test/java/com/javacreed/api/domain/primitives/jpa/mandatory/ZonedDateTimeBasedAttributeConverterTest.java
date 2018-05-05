398e86064f9f2947765dc2b8163537d2a5e83a44
Albert Attard <albert@javacreed.com>
2018-05-05 09:18:28 +0200

    Minor fixes to the zoned dates

--- src/test/java/com/javacreed/api/domain/primitives/jpa/mandatory/ZonedDateTimeBasedAttributeConverterTest.java
+++ src/test/java/com/javacreed/api/domain/primitives/jpa/mandatory/ZonedDateTimeBasedAttributeConverterTest.java
@@ -26,7 +26,6 @@ public class ZonedDateTimeBasedAttributeConverterTest {
 
   @Test
   public void test() {
-<<<<<<< HEAD
     final long millis = 1511940600000L; /* 29th November 2017 7:30am GMT (UTC/+0000) */
     final TestConverter converter = new TestConverter();
 
@@ -38,10 +37,5 @@ public class ZonedDateTimeBasedAttributeConverterTest {
     final ZoneId zone = ZoneId.of("Europe/Malta");
     final TestField write = new TestField(ZonedDateTime.of(2017, 11, 29, 8, 30, 0, 0, zone));
     Assert.assertEquals(new Timestamp(millis), converter.convertToDatabaseColumn(write));
-=======
-    final long millis = 1511940600000L; /* Wednesday, November 29, 2017 8:30:00 AM GMT+01:00 */
-    final TestField field = new TestConverter().convertToEntityAttribute(new Timestamp(millis));
-    Assert.assertEquals("2017-11-29 07:30 UTC", field.format("yyyy-MM-dd hh:mm z"));
->>>>>>> branch 'master' of https://github.com/javacreed/domain-primitives-api-jpa.git
   }
 }
